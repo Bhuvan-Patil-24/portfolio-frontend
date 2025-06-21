6ec8e2d53965a20ff447316f36521c6b52be9b6d
Bhuvan-Patil-24 <patilbhuvan27@gmail.com>
2025-06-21 17:26:00 +0530

    updated gitignore:

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-/* this function is used to keep the view on top whenever you change to different page */
 function ScrollToTop() {
   const { pathname } = useLocation();
 
